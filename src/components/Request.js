d092da4fdee8f213f85871b07452d2078c00e57a
Aung <aungheinmynn@gmail.com>
2020-12-27 09:02:07 +0800

    netlify form

--- src/components/Request.js
+++ src/components/Request.js
@@ -13,85 +13,15 @@ function Request(props) {
   const { home } = useParams();
   const { away } = useParams();
   let history = useHistory();
-  const [serverState, setServerState] = useState({
-    submitting: false,
-    status: null,
-  });
-    
-    useEffect(() => {
-      if (loading) {
-        simulateNetworkRequest().then(() => {
-          setLoading(false);
-        });
-      }
-    }, [loading]);
-  const handleServerResponse = (ok, msg, form) => {
-    setServerState({
-      submitting: false,
-      status: { ok, msg },
-    });
-    if (ok) {
-      form.reset();
-      setValidated(false);
-    }
-  };
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-      handleServerResponse(
-        false,
-        "Some error occurs while submiting. Please check your entries again.",
-        form
-      );
-    }
-
-    setValidated(true);
-    if (form.checkValidity() === true) {
-      setLoading(true);
-      setServerState({ submitting: true });
-      axios({
-        method: "post",
-        url: props.location.pathname,
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        data: new FormData(form),
-      })
-        .then((r) => {
-          handleServerResponse(
-            true,
-            "Success! Thanks for your submission.",
-            form
-          );
-          setLoading(false);
-        })
-        .catch((r) => {
-          handleServerResponse(false, r.response.data.error, form);
-        });
-    }
-  };
   return (
     <div className="form-wrapper">
-      {serverState.status && (
-        <p
-          className={
-            !serverState.status.ok
-              ? "errorMsg alert alert-danger"
-              : "alert alert-success"
-          }
-        >
-          {serverState.status.msg}
-        </p>
-      )}
       <Form
         name="requst"
         method="POST"
         data-netlify="true"
         noValidate
         validated={validated}
-        onSubmit={handleOnSubmit}
       >
         <Form.Control type="hidden" name="form-name" value="request" />
         <Form.Group controlId="formBasicEmail">
