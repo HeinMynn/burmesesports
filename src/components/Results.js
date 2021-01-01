7ef9e1b0f2b6af26979f02ebce7538ad9f4cd500
Aung <aungheinmynn@gmail.com>
2021-01-02 07:17:22 +0800

    remove ads

--- src/components/Results.js
+++ src/components/Results.js
@@ -30,10 +30,6 @@ function Results(props) {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Results</title>
-        <script
-          type="text/javascript"
-          src="//jelqr4dqeep7.com/4e/26/83/4e2683220c41b972f67aab0f2732b608.js"
-        ></script>
       </Helmet>
       <h2>Results</h2>
       <div className={loading ? "loading" : "hide"}></div>
