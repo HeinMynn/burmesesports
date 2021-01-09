9d60c042c23129d63ec15b31079291987a8c6436
Aung <aungheinmynn@gmail.com>
2021-01-10 01:05:58 +0800

    remove react-helmet

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import Iframe from "react-iframe";
 
 function Results(props) {
